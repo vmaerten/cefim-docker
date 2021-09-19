f13aae174533e62c72217f8cbe02b491d3ab92b3
Valentin Maerten <vmaerten@pubstack.io>
2021-09-19 21:17:25 +0200

    feat: add mysql and docker compose

--- provisioning.sh
+++ provisioning.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+DBHOST=localhost
+DBNAME=petclinic
+DBUSER=petclinic
+DBPASSWD=petclinic
+
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 usermod -a -G docker vagrant
@@ -6,4 +12,23 @@ usermod -a -G docker vagrant
 systemctl start docker
 systemctl enable docker
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 ufw allow 8080
+
+
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
+
+# install mysql and admin interface
+
+apt-get -y install mysql-server
+
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%' identified by '$DBPASSWD'"
+# update mysql conf file to allow remote access to the db
+
+sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+sudo service mysql restart
